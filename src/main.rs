fdfcb7765e5a3e883cd37ee9fe980460181f8a61
Karma-234 <busari1kamal@gmail.com>
2025-10-06 10:00:31 +0100

    Added reading notes

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,16 @@ fn main() {
     let tup: (bool, char, i32) = (true, 'a', 4);
     println!("bool is {}, char is {}, int is {}", tup.0, tup.1, tup.2);
 
+    // Console input
     console_input::console_input();
+
+    // Accessing sub modules
     subfolder::subfn::inner_fn();
+
+    // Arrays
+    let first_array: [i32; 5] = [1, 2, 3, 4, 5];
+
+    // We use "{:?}" to print the array in a readable format else we get a n error message
+
+    println!("First array is {:?}", first_array);
 }
